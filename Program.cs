9aeb07066abc0988b547424d047259311f455958
FLP013 <felliperibeiro9@gmail.com>
2024-09-07 00:35:25 -0300

    programa finalizado

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+ 
+Console.WriteLine(@"
+       beba  coca   cola    
+       babe         cola
+       beba  coca
+       babe  cola   caco
+       caco
+       cola
+             c l o a c a
+");
